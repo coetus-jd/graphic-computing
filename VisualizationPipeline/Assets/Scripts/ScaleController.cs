208990024844b6cbb3b35e2838d121bcfd6f5c94
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-05-05 10:27:08 -0300

    feat: object scale

---
+++ VisualizationPipeline/Assets/Scripts/ScaleController.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VisualizationPipeline.Assets.Scripts.Enums;
+
+namespace VisualizationPipeline.Assets.Scripts
+{
+    public class ScaleController : BasePipeline
+    {
+        [SerializeField] private Slider Slider;
+
+        private (float Min, float Max) Range = (1, 5);
+
+        public void ChangeObjectScale()
+        {
+            var scale = Slider.value * Range.Max;
+
+            if (scale == 0)
+                scale = Range.Min;
+
+            ObjectInPipeline.transform.localScale = new Vector3(1, 1, 1) * scale;
+        }
+    }
+}
